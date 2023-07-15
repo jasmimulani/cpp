e75f5d233fbb2fc1e5d27c1d7ad3750ca8492b1d
Jasmin <jasmimulani@gmail.com>
2023-07-15 14:53:43 +0530

    15-7-23

---
+++ pettern_5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,sp=25;
+    for(i=5; i>=1; i--)
+    {
+        for(j=sp; j>=i; j--)
+        cout<<" ";
+        for(j=i; j>=1; j--)
+        {
+
+            cout<<"  * ";
+        }
+        cout<<"\n";
+        sp++;
+    }
+}
